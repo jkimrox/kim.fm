58f1faf8ca87b8fdeb6a8a4046be23bf5ec7af7a
Jonathan <jonathankimwylie@gmail.com>
2022-05-17 08:27:37 -0500

    search component done

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import Search from './components/Search'
+import './App.css';
+
+
+
+const Home = () => {
+  return(
+    <div>
+      <Search />
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+const Search = () => {
+    return(
+        <>
+            <form>
+                <input type="text" />
+                <button>Search!</button>
+            </form>
+        </>
+    )
+}
+
+export default Search;
