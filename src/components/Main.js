ddaac42b991f6bdbc32a7e7d4eaad793cd636f52
Jonathan <jonathankimwylie@gmail.com>
2022-05-18 18:48:57 -0500

    added band description on main

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,8 +5,10 @@ const Main = (props) => {
     return (
         <section className='kim-index'>
             <h1 class="welcome-title">Welcome to Kim.fm!</h1>
-            <h2 class="main-description">Your resource for all things Steven Leftovers!</h2>
             <img src="https://f4.bcbits.com/img/a0695085979_2.jpg" alt="band"></img>
+            <h2 class="main-description">Your resource for all things Steven Leftovers!</h2>
+            <h3 class="band-description">Steven Leftovers is a Dallas, TX-based artist. His name is Evan. He is joined by Tyler (Future Nest), Matthew (Matthew Devil), and Jonathan Kim (Mouse Trap).</h3>
+            
         </section>
     )
 }
