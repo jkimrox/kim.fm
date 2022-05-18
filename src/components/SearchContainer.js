6ba2a68a790ef818796bb41319b5c3678dbfa221
Jonathan <jonathankimwylie@gmail.com>
2022-05-18 10:36:20 -0500

    got stuff displaying

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,37 +1,60 @@
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
 import Search from './Search';
 import Results from './Results';
 
 const SearchContainer = () => {
     const[query, setQuery] = useState([])
-    const[results, setResults] = useState([])
+    const[results, setResults] = useState(null)
 
-    async function handleSubmit(e) {
-        e.preventDefault()
-        // console.log(query)
+    const getData = async () => {
         try {
-            const apiKey = "ee3289f986456b1b8e715ff002105cd6"
-            const URL = `http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${query}&api_key=${apiKey}&format=json`
-
-            const response = await fetch(URL)
+            const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=Steven Leftovers&api_key=ee3289f986456b1b8e715ff002105cd6&format=json`)
             const data = await response.json()
-            setResults(data.data)
-        } catch(err) {
+            setResults(data)
+          } catch (err) {
             console.log(err)
+          }
         }
-    }
+    
+    // async function getData() {
+    //     // e.preventDefault()
+    //     // console.log(query)
+    //     try {
+    //         const apiKey = "ee3289f986456b1b8e715ff002105cd6"
+    //         const URL = `https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=cher&api_key=ee3289f986456b1b8e715ff002105cd6&format=json`
+    //     console.log(URL)
+    //         const response = await fetch(URL)
+    //         const data = await response.json()
+    //         setResults(data.data)
+    //     } catch(err) {
+    //         console.log(err)
+    //     }
+    // }
+    console.log(results)
+    useEffect(() => {getData()}, [])
+    // function handleChange(e) {
+    //     setQuery(e.target.value)
+    // }
+    
+    
 
-    function handleChange(e) {
-        setQuery(e.target.value)
-    }
-
-    return(
-        <>
-            < Search handleSubmit={handleSubmit} handleChange={handleChange} query={query} />
-            < Results results={results}/>
-        </>
-    )
-}
+    const displayData = (displayElement) =>{
+        return(
+          <>
+          <h1>{displayElement.topalbums['@attr'].artist}</h1>
+          <h2>{displayElement.topalbums.album[0].name}</h2>
+          {/* <h2>{displayElement.topalbums.album[0].image}</h2> */}
+          <h2>{displayElement.topalbums.album[1].name}</h2>
+          <h2>{displayElement.topalbums.album[2].name}</h2>
+          <h2>{displayElement.topalbums.album[3].name}</h2>
+          <h2>{displayElement.topalbums.album[4].name}</h2>
+          <h2>{displayElement.topalbums.album[5].name}</h2>
+          <h2>{displayElement.topalbums.album[6].name}</h2>
+          </>
+        )
+      };return (
+            <div>{results ? displayData(results) : <h1>Loading...</h1>}</div>
+      )
+    } 
 
 export default SearchContainer;
